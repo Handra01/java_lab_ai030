9e779cd63ecfb2848cdb9de69c03374e90d47f8b
Handra01 <162011668+Handra01@users.noreply.github.com>
2025-05-13 15:25:48 +0530

    Create lab_05b.java

---
+++ lab_05b.java
@@ -0,0 +1,55 @@
+package D_1BM23AI030;
+import java.util.Vector;
+
+class Product{
+    String name;
+    double price;
+    String category;
+    public Product(String name, double price, String category){
+        this.name= name;
+        this.price= price;
+        this.category= category;
+    }
+
+}
+public class onlineshopping {
+    private Vector<Product> inventory;
+    public onlineshopping() {
+        inventory = new Vector<>();
+    }
+    public void addProduct(String name, double price, String category){
+        Product product= new Product(name, price, category);
+        inventory.add(product);
+
+    }
+    public void removeProduct(String name){
+        for (int i=0; i<inventory.size(); i++){
+            if (inventory.get(i).name.equalsIgnoreCase(name)){
+                inventory.remove(i);
+                break;
+            }
+        }
+    }
+    public void displayInventory(){
+        System.out.println("Inventory:");
+        for (Product product : inventory){
+            System.out.println("Name:"+ product.name + ", Price:" + product.price+ ", Category:" + product.category);
+        }
+    }
+    public static void main(String[] args){
+        onlineshopping manager = new onlineshopping();
+
+        manager.addProduct("Laptop", 40000, "elctronics");
+        manager.addProduct("T-shirt", 550, "Clothing");
+        manager.addProduct("Book", 920, "Books");
+
+        manager.displayInventory();
+
+        manager.removeProduct("Book");
+
+        System.out.println("\n\nafter deletion:");
+        manager.displayInventory();
+    }
+}
+
+
